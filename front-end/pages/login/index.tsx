bb566999d0e0a519b8f0b43206dd2a3a361cf841
Lander Dirix <lander.dirix@student.ucll.be>
2024-10-29 20:55:35 +0100

    login & register page

--- front-end/pages/login/index.tsx
+++ front-end/pages/login/index.tsx
@@ -1,16 +1,40 @@
 import Head from 'next/head';
+import Link from 'next/link';
+import styles from '@styles/Login.module.css';
+import { FormEvent } from 'react';
 
-const Home: React.FC = () => {
-    return ( 
+const Login: React.FC = () => {
+    const login = (e: FormEvent) => {
+        e.preventDefault();
+        // Add your login logic here
+    };
+
+    return (
         <>
             <Head>
                 <title>Login</title>
             </Head>
             <main>
-                <p>login</p>
+                <div className={styles.container}>
+                    <div className={styles.titleContainer}>
+                        <h1>Poopedia</h1>
+                        <h3>Because every poop tells a story.</h3>
+                    </div>
+                    <div>
+                        <form onSubmit={login} className={styles.loginForm}>
+                            <h2>Login</h2>
+                            <input type="email" name="email" placeholder='Email' required />
+                            <input type="password" name="password" placeholder='Password' required />
+                            <button type="submit">LOGIN</button>
+                            <p>
+                                Don't have an account? <Link href="/register">Register here</Link>
+                            </p>
+                        </form>
+                    </div>
+                </div>
             </main>
         </>
     );
-}
+};
 
-export default Home;
+export default Login;
--- front-end/pages/register/index.tsx
+++ front-end/pages/register/index.tsx
@@ -1,16 +1,36 @@
 import Head from 'next/head';
+import Link from 'next/link';
+import styles from '@styles/Register.module.css';
+import { FormEvent } from 'react';
 
-const Home: React.FC = () => {
-    return ( 
+const Register: React.FC = () => {
+    const register = (e: FormEvent) => {
+        e.preventDefault();
+        // Add your login logic here
+    };
+
+    return (
         <>
             <Head>
                 <title>Register</title>
             </Head>
             <main>
-                <p>register</p>
+                <div className={styles.container}>
+                    <h1>Poopedia</h1>
+                    <form onSubmit={register} className={styles.registerForm}>
+                        <h2>Register</h2>
+                        <input type="text" name="username" placeholder="Username" required />
+                        <input type="email" name="email" placeholder="Email" required />
+                        <input type="password" name="password" placeholder="Password" required />
+                        <button type="submit">REGISTER</button>
+                        <p>
+                            Already have an account? <Link href="/login">Login here</Link>
+                        </p>
+                    </form>
+                </div>
             </main>
         </>
     );
-}
+};
 
-export default Home;
+export default Register;
