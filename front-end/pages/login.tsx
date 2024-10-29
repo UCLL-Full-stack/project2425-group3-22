f5a71750cf52f1b9c7774e13b29bbf8fdd8b99b3
Lander Dirix <lander.dirix@student.ucll.be>
2024-10-29 15:22:18 +0100

    front end start

---
+++ front-end/pages/login.tsx
@@ -0,0 +1,16 @@
+import Head from 'next/head';
+
+const Home: React.FC = () => {
+    return ( 
+        <>
+            <Head>
+                <title>Home</title>
+            </Head>
+            <main>
+                <p>login</p>
+            </main>
+        </>
+    );
+}
+
+export default Home;
