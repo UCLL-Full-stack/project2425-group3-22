895284a38209b037f98cd9434a34a830758f62e6
J-VD-M <r0950937@ucll.be>
2024-11-06 16:43:58 +0100

    backend: poop model validation (not all yet)

--- back-end/model/poop.ts
+++ back-end/model/poop.ts
@@ -5,36 +5,83 @@ export class Poop {
     private poopID: number;
     private type: number;
     private size: number;
-    private colorID: number;
+    private colorID?: number;
     private dateTime: Date;
-    private title: string;
-    private rating: number;
-    private latitude: number;
-    private longitude: number;
+    private title?: string;
+    private rating?: number;
+    private latitude?: number;
+    private longitude?: number;
     private user: User;
 
-    constructor(poop: {
+    constructor({
+        poopID,
+        type,
+        size,
+        colorID,
+        dateTime,
+        title,
+        rating,
+        latitude,
+        longitude,
+        user,
+    }: {
         poopID: number;
         type: number;
         size: number;
-        colorID: number;
+        colorID?: number;
         dateTime: Date;
-        title: string;
-        rating: number;
-        latitude: number;
-        longitude: number;
+        title?: string;
+        rating?: number;
+        latitude?: number;
+        longitude?: number;
         user: User;
     }) {
-        this.poopID = poop.poopID;
-        this.type = poop.type;
-        this.size = poop.size;
-        this.colorID = poop.colorID;
-        this.dateTime = poop.dateTime;
-        this.title = poop.title;
-        this.rating = poop.rating;
-        this.latitude = poop.latitude;
-        this.longitude = poop.longitude;
-        this.user = poop.user;
+        this.poopID = poopID;
+
+        if (type < 0 || type > 7)
+            throw new Error('type must be a number from 0 to 7 (0 and 7 included)');
+        this.type = type;
+
+        if (size < 0 || type > 100)
+            throw new Error('size must be a number from 0 to 100 (0 and 100 included)');
+        this.size = size;
+
+        // TODO: think about the color system (with coloring the poop in the front-end in mind)
+        if (colorID) {
+            this.colorID = colorID;
+        } else {
+            this.colorID = 0; //TODO: add default colorID here
+        }
+
+        // TODO: find out what the format from front-end is first
+        this.dateTime = dateTime;
+
+        if (title) {
+            if (title.length > 100) throw new Error('title cannot be longer than 100 characters');
+            this.title = title;
+        } else {
+            this.title = '';
+        }
+
+        this.rating = rating;
+
+        if (latitude) {
+            if (latitude < -90 || latitude > 90)
+                throw new Error('latitude must be a number between -90 and 90');
+            this.latitude = latitude;
+        } else {
+            this.latitude = undefined;
+        }
+
+        if (longitude) {
+            if (longitude < -180 || longitude > 180)
+                throw new Error('longitude must be a number between -180 and 180');
+            this.longitude = longitude;
+        } else {
+            this.longitude = undefined;
+        }
+
+        this.user = user;
     }
 
     getPoopID(): number | undefined {
