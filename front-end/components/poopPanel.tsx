e871012fa2ad793a43de8977411fc8a91f555335
Lander Dirix <lander.dirix@student.ucll.be>
2024-11-12 23:19:32 +0100

    frontend: super cool popup (cost way to much time)

--- front-end/components/poopPanel.tsx
+++ front-end/components/poopPanel.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 import styles from '@styles/poopPanel.module.css';
 import { useRouter } from 'next/router';
 import mapboxgl from 'mapbox-gl';
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 type Props = {
@@ -11,9 +11,13 @@ type Props = {
 };
 
 const PoopPanel: React.FC<Props> = ({ poop }: Props) => {
-    const mapContainerRef = useRef<HTMLDivElement | null>(null);
-    const firstLetter = poop.user.username.charAt(0).toUpperCase();
+    const [popupPosition, setPopupPosition] = useState<{ x: number; y: number } | null>(null);
+    const [showPopup, setShowPopup] = useState(false);
 
+    const mapContainerRef = useRef<HTMLDivElement | null>(null);
+    const popupRef = useRef<HTMLDivElement | null>(null);
+
+    const firstLetter = poop.user.username.charAt(0).toUpperCase();
 
     useEffect(() => {
         mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOXGL_ACCESS_TOKEN ?? '';
@@ -32,40 +36,101 @@ const PoopPanel: React.FC<Props> = ({ poop }: Props) => {
                 attributionControl: false,
             });
 
-
-            const marker = new mapboxgl.Marker({ color: 'brown' }).setLngLat([poop.longitude, poop.latitude]).addTo(map);
+            const marker = new mapboxgl.Marker({ color: 'brown' })
+                .setLngLat([poop.longitude, poop.latitude])
+                .addTo(map);
 
             return () => map.remove();
         }
     }, []);
 
+    useEffect(() => {
+        // when the popup is shown -> hide it when scrolling or resizing
+        const handleClosePopup = () => {
+            setShowPopup(false);
+        };
+
+        // when clicking outside the popup -> close it
+        const handleClickOutsidePopup = (event: MouseEvent) => {
+            if (showPopup) {
+                if (popupRef.current && !popupRef.current.contains(event.target as Node)) {
+                    setShowPopup(false);
+                }
+            }
+        };
+
+        if (showPopup) {
+            window.addEventListener('scroll', handleClosePopup);
+            window.addEventListener('resize', handleClosePopup);
+
+            // set timeout to prevent the click event from closing the popup immediately
+            setTimeout(() => {
+                window.addEventListener('click', handleClickOutsidePopup);
+            }, 100);
+        }
+
+        return () => {
+            window.removeEventListener('scroll', handleClosePopup);
+            window.removeEventListener('resize', handleClosePopup);
+            window.removeEventListener('click', handleClickOutsidePopup);
+        };
+    }, [showPopup]);
+
+    const handleOptionsClick = (event: React.MouseEvent) => {
+        console.log('open popup');
+        const { clientX, clientY } = event;
+        setPopupPosition({ x: clientX, y: clientY });
+        setShowPopup(true);
+    };
+
     return (
-        <div className={styles.poopItem}>
-            <div className={styles.nameContainer}>
-                <div className={styles.userIcon}>{firstLetter}</div>
-                <div>
-                    <p>{poop.user.username}</p>
-                    <p>
-                        {new Date(poop.dateTime).toLocaleString('en-GB', {
-                            day: '2-digit',
-                            month: '2-digit',
-                            year: 'numeric',
-                            hour: '2-digit',
-                            minute: '2-digit',
-                        })}
-                    </p>
+        <>
+            <div className={styles.poopItem}>
+                <div className={styles.nameContainer}>
+                    <div className={styles.userIcon}>{firstLetter}</div>
+                    <div>
+                        <p>{poop.user.username}</p>
+                        <p>
+                            {new Date(poop.dateTime).toLocaleString('en-GB', {
+                                day: '2-digit',
+                                month: '2-digit',
+                                year: 'numeric',
+                                hour: '2-digit',
+                                minute: '2-digit',
+                            })}
+                        </p>
+                    </div>
+                    <button className={styles.optionsButton} onClick={handleOptionsClick}>
+                        <FontAwesomeIcon icon="ellipsis-vertical" />
+                    </button>
                 </div>
-                <button className={styles.optionsButton}><FontAwesomeIcon icon="ellipsis-vertical"/></button>
+                <h2>{poop.title}</h2>
+
+                <p>rating: {poop.rating}</p>
+                <p>size: {poop.size}</p>
+                <p>colorID: {poop.colorID}</p>
+                <p>type: {poop.type}</p>
+
+                <div ref={mapContainerRef} className={styles.map}></div>
             </div>
-            <h2>{poop.title}</h2>
-
-            <p>rating: {poop.rating}</p>
-            <p>size: {poop.size}</p>
-            <p>colorID: {poop.colorID}</p>
-            <p>type: {poop.type}</p>
-
-            <div ref={mapContainerRef} className={styles.map}></div>
-        </div>
+            {showPopup && popupPosition && (
+                <div
+                    className={styles.popup}
+                    ref={popupRef}
+                    style={{
+                        left: `${popupPosition.x}px`,
+                        top: `${popupPosition.y}px`,
+                    }}
+                >
+                    <ul>
+                        <li>Edit</li>
+                        <li>Delete</li>
+                        <li>Remove Friend</li>
+                        <li>Another thing</li>
+                    </ul>
+                </div>
+            )}
+        </>
     );
 };
 
