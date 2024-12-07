ea96297f20c6eb3483905957a75f8da1847ced4f
Lander Dirix <lander.dirix@student.ucll.be>
2024-12-07 21:46:05 +0100

    frontend: updated actionmenu in the poopPanel

---
+++ front-end/components/ActionMenus/editPoopActionMenu.tsx
@@ -0,0 +1,56 @@
+import { useEffect, useRef, useState } from 'react';
+
+type Props = {
+    popupPosition: { x: number; y: number };
+    setShowPopup: (show: boolean) => void;
+};
+
+const EditPoopActionMenu: React.FC<Props> = ({ popupPosition, setShowPopup }: Props) => {
+    const popupRef = useRef<HTMLDivElement | null>(null);
+
+    useEffect(() => {
+        // Close popup on scroll or resize
+        const handleClosePopup = () => setShowPopup(false);
+
+        // when clicking outside the popup -> close it
+        const handleClickOutsidePopup = (event: MouseEvent) => {
+            if (popupRef.current && !popupRef.current.contains(event.target as Node)) {
+                handleClosePopup();
+            }
+        };
+
+        // Close popup if clicking outside of it
+
+        window.addEventListener('scroll', handleClosePopup);
+        window.addEventListener('resize', handleClosePopup);
+
+        // Add click event listener to handle closing popup
+        setTimeout(() => {
+            window.addEventListener('click', handleClickOutsidePopup);
+        }, 0);
+
+        return () => {
+            window.removeEventListener('scroll', handleClosePopup);
+            window.removeEventListener('resize', handleClosePopup);
+            window.removeEventListener('click', handleClickOutsidePopup);
+        };
+    }, [setShowPopup]);
+
+    return (
+        <div
+            className="popup"
+            ref={popupRef}
+            style={{
+                left: `${popupPosition.x}px`,
+                top: `${popupPosition.y}px`,
+            }}
+        >
+            <button>Edit</button>
+            <button>Delete</button>
+            <button>Remove Friend</button>
+            <button>Another thing</button>
+        </div>
+    );
+};
+
+export default EditPoopActionMenu;
--- front-end/components/poopPanel.tsx
+++ front-end/components/poopPanel.tsx
@@ -1,11 +1,10 @@
 import { poopItem } from '@types';
-import Link from 'next/link';
 import styles from '@styles/poopPanel.module.css';
-import { useRouter } from 'next/router';
 import mapboxgl from 'mapbox-gl';
 import { useEffect, useRef, useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Rate from 'rc-rate';
+import EditPoopActionMenu from './ActionMenus/editPoopActionMenu';
 
 type Props = {
     poop: poopItem;
@@ -16,8 +15,6 @@ const PoopPanel: React.FC<Props> = ({ poop }: Props) => {
     const [showPopup, setShowPopup] = useState(false);
 
     const mapContainerRef = useRef<HTMLDivElement | null>(null);
-    const popupRef = useRef<HTMLDivElement | null>(null);
-
     const firstLetter = poop.user.username.charAt(0).toUpperCase();
 
     useEffect(() => {
@@ -48,38 +45,6 @@ const PoopPanel: React.FC<Props> = ({ poop }: Props) => {
             return () => map.remove();
         }
     }, []);
-
-    useEffect(() => {
-        // when the popup is shown -> hide it when scrolling or resizing
-        const handleClosePopup = () => {
-            setShowPopup(false);
-        };
-
-        // when clicking outside the popup -> close it
-        const handleClickOutsidePopup = (event: MouseEvent) => {
-            if (showPopup) {
-                if (popupRef.current && !popupRef.current.contains(event.target as Node)) {
-                    setShowPopup(false);
-                }
-            }
-        };
-
-        if (showPopup) {
-            window.addEventListener('scroll', handleClosePopup);
-            window.addEventListener('resize', handleClosePopup);
-
-            // set timeout to prevent the click event from closing the popup immediately
-            setTimeout(() => {
-                window.addEventListener('click', handleClickOutsidePopup);
-            }, 0);
-        }
-
-        return () => {
-            window.removeEventListener('scroll', handleClosePopup);
-            window.removeEventListener('resize', handleClosePopup);
-            window.removeEventListener('click', handleClickOutsidePopup);
-        };
-    }, [showPopup]);
 
     const handleOptionsClick = (event: React.MouseEvent) => {
         const { clientX, clientY } = event;
@@ -112,7 +77,7 @@ const PoopPanel: React.FC<Props> = ({ poop }: Props) => {
             <p>size: {poop.size}</p>
             <p>colorID: {poop.colorID}</p>
             <p>type: {poop.type}</p>
-            <Rate 
+            <Rate
                 count={5}
                 value={poop.rating}
                 allowHalf={true}
@@ -123,19 +88,7 @@ const PoopPanel: React.FC<Props> = ({ poop }: Props) => {
                 <div ref={mapContainerRef} className={styles.map}></div>
             )}
             {showPopup && popupPosition && (
-                <div
-                    className="popup"
-                    ref={popupRef}
-                    style={{
-                        left: `${popupPosition.x}px`,
-                        top: `${popupPosition.y}px`,
-                    }}
-                >
-                    <button>Edit</button>
-                    <button>Delete</button>
-                    <button>Remove Friend</button>
-                    <button>Another thing</button>
-                </div>
+                <EditPoopActionMenu popupPosition={popupPosition} setShowPopup={setShowPopup} />
             )}
         </div>
     );
