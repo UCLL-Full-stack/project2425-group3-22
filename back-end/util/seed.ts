6c9a8a1ca304cb576f8545cf33b3f5ee75435049
J-VD-M <r0950937@ucll.be>
2024-12-15 00:33:12 +0100

    backend: dummy data

--- back-end/util/seed.ts
+++ back-end/util/seed.ts
@@ -12,6 +12,7 @@ const main = async () => {
     await prisma.user.deleteMany();
     await prisma.userAchievements.deleteMany();
 
+    //#region USERS
     const admin = await prisma.user.create({
         data: {
             username: 'Admin',
@@ -66,7 +67,17 @@ const main = async () => {
             role: 'USER',
         },
     });
+    const user5 = await prisma.user.create({
+        data: {
+            username: 'user5',
+            email: 'user5@poopedia.com',
+            password: await bcrypt.hash('User5!123', 12),
+            role: 'USER',
+        },
+    });
+    //#endregion
 
+    //#region POOPS
     await prisma.poop.create({
         data: {
             dateTime: new Date(),
@@ -74,7 +85,7 @@ const main = async () => {
             size: 50,
             rating: 4,
             colorID: 0,
-            title: 'poop1',
+            title: 'Poop and Scoot.',
             longitude: 4.724166822996737,
             latitude: 50.880434297929774,
             user: { connect: { userID: user1.userID } },
@@ -87,7 +98,7 @@ const main = async () => {
             size: 50,
             rating: 4,
             colorID: 0,
-            title: 'poop1',
+            title: 'Drop and Drive.',
             longitude: null,
             latitude: null,
             user: { connect: { userID: user1.userID } },
@@ -100,7 +111,7 @@ const main = async () => {
             size: 80,
             rating: 2,
             colorID: 0,
-            title: 'Big poop',
+            title: 'Incontinentia Pooptocks.',
             longitude: 4.731953220736576,
             latitude: 50.93708586970979,
             user: { connect: { userID: user2.userID } },
@@ -113,13 +124,93 @@ const main = async () => {
             size: 80,
             rating: 2,
             colorID: 0,
-            title: 'Big poop',
+            title: 'Biggus Poopus.',
             longitude: null,
             latitude: null,
             user: { connect: { userID: user2.userID } },
         },
     });
+    await prisma.poop.create({
+        data: {
+            dateTime: new Date(),
+            type: 1,
+            size: 10,
+            rating: 3,
+            colorID: 0,
+            title: 'Rip ass and gass.',
+            longitude: 4.824166822996737,
+            latitude: 50.280434297929774,
+            user: { connect: { userID: user3.userID } },
+        },
+    });
+    await prisma.poop.create({
+        data: {
+            dateTime: new Date(),
+            type: 1,
+            size: 60,
+            rating: 4.5,
+            colorID: 0,
+            title: 'Shart and depart.',
+            longitude: null,
+            latitude: null,
+            user: { connect: { userID: user3.userID } },
+        },
+    });
+    await prisma.poop.create({
+        data: {
+            dateTime: new Date(),
+            type: 3,
+            size: 74,
+            rating: 2.5,
+            colorID: 0,
+            title: 'Shit and Skeddadle.',
+            longitude: 4.554166822996737,
+            latitude: 50.550434297929774,
+            user: { connect: { userID: user4.userID } },
+        },
+    });
+    await prisma.poop.create({
+        data: {
+            dateTime: new Date(),
+            type: 3,
+            size: 2,
+            rating: 5,
+            colorID: 0,
+            title: 'Leave a big one and go away man.',
+            longitude: null,
+            latitude: null,
+            user: { connect: { userID: user4.userID } },
+        },
+    });
+    await prisma.poop.create({
+        data: {
+            dateTime: new Date(),
+            type: 5,
+            size: 99,
+            rating: 3.5,
+            colorID: 0,
+            title: 'Push one out and go about.',
+            longitude: 4.744166822996737,
+            latitude: 50.895434297929774,
+            user: { connect: { userID: user5.userID } },
+        },
+    });
+    await prisma.poop.create({
+        data: {
+            dateTime: new Date(),
+            type: 5,
+            size: 43,
+            rating: 4,
+            colorID: 0,
+            title: 'Drop a bomb and leave therefrom.',
+            longitude: null,
+            latitude: null,
+            user: { connect: { userID: user5.userID } },
+        },
+    });
+    //#endregion
 
+    //#region FRIENDS
     await prisma.friendRequest.create({
         data: {
             sender: { connect: { userID: user1.userID } },
@@ -139,6 +230,13 @@ const main = async () => {
             user2: { connect: { userID: user4.userID } },
         },
     });
+    await prisma.friends.create({
+        data: {
+            user1: { connect: { userID: user5.userID } },
+            user2: { connect: { userID: user1.userID } },
+        },
+    });
+    //#endregion
 };
 
 (async () => {
